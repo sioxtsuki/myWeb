d8fe06eae9731e9fb3df0381c1154b6bebc0ce32
俊広 塩月 <shiotsuki@worldmarksolutions.com>
2020-08-18 11:38:01 +0900

    live1 delete

--- src/main/java/com/main/LineBotController.java
+++ src/main/java/com/main/LineBotController.java
@@ -5,11 +5,34 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author shiotsuki
+ *
+ */
 @RestController
 public class LineBotController {
 
+	//@Autowired
+	//private LineMessagingClient lineMessagingClient;
+
+
 	@RequestMapping(value = "/linebot")
-	String index(HttpServletRequest request) throws RuntimeException {
-		return "OK";
+	void index(HttpServletRequest request) throws RuntimeException {
+
+		System.out.println("request: " + request.getParameter("text").toString());
+/*
+		@SuppressWarnings("unused")
+		BotApiResponse response;
+		try {
+			response = this.lineMessagingClient
+			        .pushMessage(new PushMessage("Ud6e699869888126beedab30c5b3d484e".toString(),
+			                     new TextMessage(request.getParameter("text").toString()
+			                      ))).get();
+
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		*/
 	}
 }
